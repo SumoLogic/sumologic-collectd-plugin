2740775e9e80df49185c1e4ab68c256621b32360
Chuck Che <cche@sumologic.com>
2017-08-03 21:11:09 -0700

    Test code optimize

--- test/test_metrics_config.py
+++ test/test_metrics_config.py
@@ -43,36 +43,6 @@ def test_parse_meta_tags():
     assert met_config.conf[ConfigOptions.meta_tags] == tuple_to_dict(tags)
 
 
-def test_parse_source_name():
-    met_config = MetricsConfig()
-    source_name = 'test_source'
-    source_name_node = ConfigNode(ConfigOptions.source_name, [source_name])
-    config = CollectdConfig([Helper.url_node(), source_name_node])
-    met_config.parse_config(config)
-
-    assert met_config.conf[ConfigOptions.source_name] == source_name
-
-
-def test_parse_host_name():
-    met_config = MetricsConfig()
-    host_name = 'test_host'
-    host_name_node = ConfigNode(ConfigOptions.host_name, [host_name])
-    config = CollectdConfig([Helper.url_node(), host_name_node])
-    met_config.parse_config(config)
-
-    assert met_config.conf[ConfigOptions.host_name] == host_name
-
-
-def test_parse_source_category():
-    met_config = MetricsConfig()
-    source_category = 'test_category'
-    source_category_node = ConfigNode(ConfigOptions.source_category, [source_category])
-    config = CollectdConfig([Helper.url_node(), source_category_node])
-    met_config.parse_config(config)
-
-    assert met_config.conf[ConfigOptions.source_category] == source_category
-
-
 def test_parse_http_post_interval():
     met_config = MetricsConfig()
     http_post_interval = '0.5'
@@ -83,89 +53,44 @@ def test_parse_http_post_interval():
     assert met_config.conf[ConfigOptions.http_post_interval] == float(http_post_interval)
 
 
+def test_parse_string_config():
+    met_config = MetricsConfig()
+
+    configs = {
+        'source_name': 'test_source',
+        'host_name': 'test_host',
+        'source_category': 'test_category'
+    }
+
+    for (key, value) in configs.items():
+        node = ConfigNode(getattr(ConfigOptions, key), [value])
+        config = CollectdConfig([Helper.url_node(), node])
+        met_config.parse_config(config)
+
+        assert met_config.conf[getattr(ConfigOptions, key)] == value
+
+
 def test_parse_int_config():
     met_config = MetricsConfig()
 
-    max_batch_size = '10'
-    max_batch_interval = '5'
-    retry_initial_delay = '2'
-    retry_max_attempts = '5'
-    retry_max_delay = '10'
-    retry_backoff = '3'
-    retry_jitter_min = '1'
-    retry_jitter_max = '5'
-    max_requests_to_buffer = '100'
+    configs = {
+        'max_batch_size': '10',
+        'max_batch_interval': '5',
+        'retry_initial_delay': '2',
+        'retry_max_attempts': '5',
+        'retry_max_delay': '10',
+        'retry_backoff': '3',
+        'retry_jitter_min': '1',
+        'retry_jitter_max': '5',
+        'max_requests_to_buffer': '100'
+    }
 
-    max_batch_size_node = ConfigNode(ConfigOptions.max_batch_size, [max_batch_size])
-    max_batch_interval_node = ConfigNode(ConfigOptions.max_batch_interval, [max_batch_interval])
-    retry_initial_delay_node = ConfigNode(ConfigOptions.retry_initial_delay, [retry_initial_delay])
-    retry_max_attempts_node = ConfigNode(ConfigOptions.retry_max_attempts, [retry_max_attempts])
-    retry_max_delay_node = ConfigNode(ConfigOptions.retry_max_delay, [retry_max_delay])
-    retry_backoff_node = ConfigNode(ConfigOptions.retry_backoff, [retry_backoff])
-    retry_jitter_min_node = ConfigNode(ConfigOptions.retry_jitter_min, [retry_jitter_min])
-    retry_jitter_max_node = ConfigNode(ConfigOptions.retry_jitter_max, [retry_jitter_max])
-    max_requests_to_buffer_node = ConfigNode(ConfigOptions.max_requests_to_buffer, [max_requests_to_buffer])
+    for (key, value) in configs.items():
+        node = ConfigNode(getattr(ConfigOptions, key), [value])
+        config = CollectdConfig([Helper.url_node(), node])
+        met_config.parse_config(config)
 
-    config = CollectdConfig([Helper.url_node(), max_batch_size_node, max_batch_interval_node,
-                             retry_initial_delay_node, retry_max_attempts_node,
-                             retry_max_delay_node, retry_backoff_node, retry_jitter_min_node,
-                             retry_jitter_max_node, max_requests_to_buffer_node])
-
-    met_config.parse_config(config)
-
-    assert met_config.conf[ConfigOptions.max_batch_size] == int(max_batch_size)
-    assert met_config.conf[ConfigOptions.max_batch_interval] == int(max_batch_interval)
-    assert met_config.conf[ConfigOptions.retry_initial_delay] == int(retry_initial_delay)
-    assert met_config.conf[ConfigOptions.retry_max_attempts] == int(retry_max_attempts)
-    assert met_config.conf[ConfigOptions.retry_max_delay] == int(retry_max_delay)
-    assert met_config.conf[ConfigOptions.retry_backoff] == int(retry_backoff)
-    assert met_config.conf[ConfigOptions.retry_jitter_min] == int(retry_jitter_min)
-    assert met_config.conf[ConfigOptions.retry_jitter_max] == int(retry_jitter_max)
-    assert met_config.conf[ConfigOptions.max_requests_to_buffer] == int(max_requests_to_buffer)
-
-
-def test_parse_batch_config():
-    met_config = MetricsConfig()
-
-    max_batch_size = '10'
-    max_batch_interval = '5'
-    max_batch_size_node = ConfigNode(ConfigOptions.max_batch_size, [max_batch_size])
-    max_batch_interval_node = ConfigNode(ConfigOptions.max_batch_interval, [max_batch_interval])
-    config = CollectdConfig([Helper.url_node(), max_batch_size_node, max_batch_interval_node])
-
-    met_config.parse_config(config)
-
-    assert met_config.conf[ConfigOptions.max_batch_size] == int(max_batch_size)
-    assert met_config.conf[ConfigOptions.max_batch_interval] == int(max_batch_interval)
-
-
-def test_parse_retry_config():
-    met_config = MetricsConfig()
-
-    retry_initial_delay = '2'
-    retry_max_attempts = '5'
-    retry_max_delay = '10'
-    retry_backoff = '3'
-    retry_jitter_min = '1'
-    retry_jitter_max = '5'
-
-    retry_initial_delay_node = ConfigNode(ConfigOptions.retry_initial_delay, [retry_initial_delay])
-    retry_max_attempts_node = ConfigNode(ConfigOptions.retry_max_attempts, [retry_max_attempts])
-    retry_max_delay_node = ConfigNode(ConfigOptions.retry_max_delay, [retry_max_delay])
-    retry_backoff_node = ConfigNode(ConfigOptions.retry_backoff, [retry_backoff])
-    retry_jitter_min_node = ConfigNode(ConfigOptions.retry_jitter_min, [retry_jitter_min])
-    retry_jitter_max_node = ConfigNode(ConfigOptions.retry_jitter_max, [retry_jitter_max])
-    config = CollectdConfig([Helper.url_node(), retry_initial_delay_node, retry_max_attempts_node,
-                             retry_max_delay_node, retry_backoff_node, retry_jitter_min_node,
-                             retry_jitter_max_node])
-
-    met_config.parse_config(config)
-
-    assert met_config.conf[ConfigOptions.retry_initial_delay] == int(retry_initial_delay)
-    assert met_config.conf[ConfigOptions.retry_max_attempts] == int(retry_max_attempts)
-    assert met_config.conf[ConfigOptions.retry_max_delay] == int(retry_max_delay)
-    assert met_config.conf[ConfigOptions.retry_backoff] == int(retry_backoff)
-    assert met_config.conf[ConfigOptions.retry_jitter_min] == int(retry_jitter_min)
+        assert met_config.conf[getattr(ConfigOptions, key)] == int(value)
 
 
 def test_parse_string_exception():
