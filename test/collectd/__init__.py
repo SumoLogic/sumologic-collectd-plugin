11590181b7b1e0669380a3269c32f0e80c29cca5
Chuck Che <cche@sumologic.com>
2017-08-08 18:21:15 -0700

    Remove all source and test codes for cleaner PR submission/review

--- test/collectd/__init__.py
+++
@@ -1,3 +0,0 @@
-from register import (register_config, register_init, register_read, register_write)
-
-from logger import (debug, info, warning, error)
--- test/requests/__init__.py
+++
@@ -1,3 +0,0 @@
-from post import (post, post_response_decider, mock_server, mock_response)
-
-import exceptions
